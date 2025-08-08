3dd3d87b3befc273451dfb70c4c14322dc0cf3b9
minhnguyen8989 <nguyen.dev1993@gmail.com>
2025-08-08 12:42:39 -0500

    first commit

---
+++ src/test/java/com/minhnguyen/springbootproject2/SpringbootProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhnguyen.springbootproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
